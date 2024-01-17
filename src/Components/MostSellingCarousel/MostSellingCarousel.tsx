cbb1cd04f0c6a60a24ebbc235da12332c143befd
SteveLorde <mostafa.maher98@gmail.com>
2024-01-17 22:04:22 +0200

    moved most selling carousel into seperate component (server component)

---
+++ src/Components/MostSellingCarousel/MostSellingCarousel.tsx
@@ -0,0 +1,29 @@
+'use server'
+
+
+import style from "@/app/Homestyle.module.css";
+import {Product} from "@/Data/Models/Product";
+import Link from "next/link";
+import * as backendservice from "@/Services/DataAPI/DataAPIService";
+
+export async function MostSellingCarousel() {
+
+    let mostselling24 = await backendservice.GetMostSelling();
+
+    return <>
+        <div className={style.mostsellingcanvaas}>
+            <h3 className={style.mostsellingheader}>24Hrs Most Selling</h3>
+            <div className={style.movingcards}>
+                {mostselling24?.map((product: Product) =>
+                    <Link key={product.id} href={`/Shop/Product/${product.id}`} className={style.productcard}>
+                        <img className={style.productimage}
+                             src={`${backendservice.apiurl}/storage/EShopApp/Products/${product.id}/Images/${product.images[0]}`}></img>
+                        <h2 className={style.cardtext}>{product.name}</h2>
+                        <h3 className={style.cardprice}>{product.price} Egp</h3>
+                    </Link>
+                )}
+            </div>
+        </div>
+    </>
+
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,46 +7,19 @@ import {useEffect, useState} from "react";
 import { Carousel } from "react-bootstrap";
 import Link from "next/link";
 import {NewsCarousel} from "@/Components/NewsCarousel/newscarousel";
+import {MostSellingCarousel} from "@/Components/MostSellingCarousel/MostSellingCarousel";
 
 export default function Home() {
-    const [mostselling24, setMostSelling] = useState<Product[]>([])
-
-
-
-
-        async function GetMostSelling() {
-            let mostsellings : Product[] | any = await backendservice.GetMostSelling()
-            setMostSelling(mostsellings)
-        }
-
-
-
-        useEffect(() => {
-            GetMostSelling()
-        }, []);
-
 
         return <>
             <div className={style.homelayout}>
                 {/* News Canvas */}
                 <NewsCarousel />
 
-
                 {/* Most Selling past 24 hours */}
-
-                <div className={style.mostsellingcanvaas}>
-                    <h3 className={style.mostsellingheader}>24Hrs Most Selling</h3>
-                    <div className={style.movingcards}>
-                    {mostselling24?.map((product: Product) =>
-                        <Link  key={product.id} href={`/Shop/Product/${product.id}`} className={style.productcard}>
-                            <img className={style.productimage} src={`${backendservice.apiurl}/storage/EShopApp/Products/${product.id}/Images/${product.images[0]}`}></img>
-                            <h2 className={style.cardtext}>{product.name}</h2>
-                            <h3 className={style.cardprice}>{product.price} Egp</h3>
-                        </Link>
-                    )}
-                    </div>
-                </div>
+                <MostSellingCarousel />
             </div>
+
 
         </>
 }
